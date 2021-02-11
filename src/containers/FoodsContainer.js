6d4229e926c32f11a210609e36562142a7eef6f1
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2021-02-11 14:10:26 +0500

    Fixed Pagination

--- src/containers/FoodsContainer.js
+++ src/containers/FoodsContainer.js
@@ -7,7 +7,7 @@ import Loading from '../component/Loading';
 const FoodsContainer = ({
   category, foods, error, loading,
 }) => {
-  const [activePage, setActivePage] = useState(5);
+  const [activePage, setActivePage] = useState(1);
   const [startPoint, setStartPoint] = useState(0);
   const [endPoint, setEndPoint] = useState(4);
   let data;
@@ -15,8 +15,8 @@ const FoodsContainer = ({
   let paginationTotalElements;
   const handlePageChange = pageNumber => {
     setActivePage(pageNumber);
-    setStartPoint(pageNumber * 4);
-    setEndPoint((pageNumber + 1) * 4);
+    setStartPoint((pageNumber - 1) * 4);
+    setEndPoint(pageNumber * 4);
   };
   if (loading) {
     return (
@@ -28,6 +28,7 @@ const FoodsContainer = ({
     data = foods;
     if (category === 'All' || '') {
       paginationTotalElements = foods.length;
+
       filterBooks = data.slice(startPoint, endPoint).map(food => (
         <Foods
           key={food.idMeal}
@@ -50,15 +51,20 @@ const FoodsContainer = ({
     }
   }
   return (
-    <div className="foodsContainer">
-      {filterBooks}
+    <div>
+      <div className="foodsContainer">
+        {filterBooks}
+      </div>
       <div>
         <Pagination
           activePage={activePage}
           itemsCountPerPage={4}
           totalItemsCount={paginationTotalElements}
+          itemClass="paginationLi"
+          linkClass="paginationLink"
           pageRangeDisplayed={3}
           onChange={handlePageChange}
+          activeClass="active"
         />
       </div>
     </div>
